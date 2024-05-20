d65a804a1fba55651675d800e322559a36449e21
ftounalt <168537985+ftounalt@users.noreply.github.com>
2024-05-20 17:30:37 +0300

    Update _final2 (1).py

--- _final2 (1).py
+++ _final2 (1).py
@@ -7,7 +7,7 @@ import pickle
 
 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -38,36 +38,15 @@ y = dataset.iloc[:, -1].values
 
 x_train,x_test,y_train,y_test=train_test_split(X,y)
 
-param_grid = {
-    'n_estimators': [100, 200, 300],
-    'learning_rate': [0.01, 0.05, 0.1],
-    'max_depth': [2, 3, 4],
-    'subsample': [0.8, 1.0]
-}
-
-grid_search = GridSearchCV(GradientBoostingClassifier(), param_grid, cv=5)
-grid_search.fit(x_train, y_train)
-
-best_params = grid_search.best_params_
-
-model_gb_tuned = GradientBoostingClassifier(
-    n_estimators=best_params['n_estimators'],
-    learning_rate=best_params['learning_rate'],
-    max_depth=best_params['max_depth'],
-    subsample=best_params['subsample']
-)
-
-model_gb_tuned.fit(x_train, y_train)
-predictions_tuned_gb = model_gb_tuned.predict(x_test)
-
-training_score = model_gb_tuned.score(x_train, y_train)
-test_score = model_gb_tuned.score(x_test, y_test)
+svm = SVC(kernel='rbf' , gamma=0.1 , C=10)
+svm.fit(X_train, y_train)
+y_preds = svm.predict(X_test)
 
 
 
 
 
-pickle.dump(model_gb_tuned,open('gb_model.pkl','wb'))
+pickle.dump(svm,open('gb_model.pkl','wb'))
 
 
 
